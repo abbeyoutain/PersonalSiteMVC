3b975a8fd0595f2d544287034cb19175106e3e29
Abbey Outain <abbeyoutain@outlook.com>
2022-01-13 01:24:43 -0600

    Began converting HTML Personal Site to MVC. Made basic _Layout.

---
+++ PersonalSiteMVC.UI/_Archive/assets/js/Validation.js
@@ -0,0 +1,8 @@
+﻿//Utilizing the code completed together in class as a guide,
+//build a contact form that includes all of the elements listed below:
+//Utilize JavaScript to require that a user submits information in each field.
+//Utilize a regular expression to ensure a valid email address is submitted.
+
+function validateForm(event) {
+
+}
