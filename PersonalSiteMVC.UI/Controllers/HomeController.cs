b72f65ea42c92b22238ff2d0700e30b9bc334315
abbeyoutain <abbeyoutain@outlook.com>
2022-02-20 02:35:34 -0600

    Added Contact Form functionality

--- PersonalSiteMVC.UI/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PersonalSiteMVC.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -32,9 +35,47 @@ namespace PersonalSiteMVC.UI.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                //send the cvm back to the form with completed inputs
+                return View(cvm);
+            }
+            //build the message
+            string message = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+            //MailMessage - What sends the email
+            MailMessage mm = new MailMessage("admin@abbey-outain.com", "abbeyoutain@outlook.com", cvm.Subject, message);
+
+            //MailMessage properties
+            //Allow HTML in the email
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            //Respond to the sender, and not our website
+            mm.ReplyToList.Add(cvm.Email);
+
+            //SmtpClient - This is the info from the host that allows this to be sent
+            SmtpClient client = new SmtpClient("mail.abbey-outain.com");
+
+            //Client Credentials
+            client.Credentials = new NetworkCredential("admin@abbey-outain.com", "P@ssw0rd");
+
+            //Try to send the email
+            try
+            {
+                //attempt to send
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry your request could not be completed at this time. Please try again later. Error Message:<br/>{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
         }
     }
 }
---
+++ PersonalSiteMVC.UI/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //added
+
+namespace PersonalSiteMVC.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
